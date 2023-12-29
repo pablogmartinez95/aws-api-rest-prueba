b627f96b819a880691e47935f6c2cf906838061e
Pablo Martinez <pablogmartinez95@gmail.com>
2023-12-29 12:19:29 -0300

    cors

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+var cors = require('cors')
+
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: '*'
+}));
 
 const ITEMS_LIST = [];
 
