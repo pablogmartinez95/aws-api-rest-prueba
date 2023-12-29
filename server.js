7171c6d323f510e7da3805fb4405b5aaa230aa83
Pablo Martinez <pablogmartinez95@gmail.com>
2023-12-29 12:13:00 -0300

    nuevos cambios

--- server.js
+++ server.js
@@ -12,12 +12,17 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     const item = req.body.item;
 
+    console.log(`El item es ${item}`);
+
     ITEMS_LIST.push({
         id: ITEMS_LIST.length + 1,
         item
     });
 
-    res.send(ITEMS_LIST);
+    res.send({
+        status: 200,
+        message: 'Item agregado correctamente'
+    });
 });
 
 app.listen(3000, () => {
